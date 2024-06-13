29974c53eb105eea8d949ad435187105f5d57aa9
TooWorthless <91129167+TooWorthless@users.noreply.github.com>
2024-06-13 22:58:59 +0300

    Update code

--- src/app/routers/categories.router.js
+++ src/app/routers/categories.router.js
@@ -1,17 +1,36 @@
 import { Router } from 'express';
 import categoriesController from '../controllers/categories.controller.js';
-import { jwtAuthMiddleware } from '../middlewares/auth.middleware.js';
+import { jwtAuthMiddleware, rbacAuthMiddleware } from '../middlewares/auth.middleware.js';
+import { validateMiddleware } from '../middlewares/validate.middleware.js';
+import { categorySchema } from '../../mongo/schemas/validationSchemas/validationSchemas.js';
 
 
 const categoriesRouter = Router();
 
 
 categoriesRouter.use(jwtAuthMiddleware);
+
 categoriesRouter.get('/', categoriesController.getCategories);
+
 categoriesRouter.get('/:id', categoriesController.getCategoryById);
-categoriesRouter.post('/', categoriesController.createCategory);
-categoriesRouter.put('/:id', categoriesController.updateCategory);
-categoriesRouter.delete('/:id', categoriesController.deleteCategory);
+
+categoriesRouter.post('/', 
+    rbacAuthMiddleware('admin'), 
+    validateMiddleware(categorySchema),
+    categoriesController.createCategory
+);
+
+categoriesRouter.put('/:id', 
+    rbacAuthMiddleware('admin'), 
+    validateMiddleware(categorySchema),
+    categoriesController.updateCategory
+);
+
+categoriesRouter.delete('/:id', 
+    rbacAuthMiddleware('admin'), 
+    validateMiddleware(categorySchema),
+    categoriesController.deleteCategory
+);
 
 
 export default categoriesRouter;
--- src/app/routers/products.router.js
+++ src/app/routers/products.router.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controller.js';
-import { jwtAuthMiddleware } from '../middlewares/auth.middleware.js';
-import { rbacAuthMiddleware } from '../middlewares/auth.middleware.js';
+import { jwtAuthMiddleware, rbacAuthMiddleware } from '../middlewares/auth.middleware.js';
 import { validateMiddleware } from '../middlewares/validate.middleware.js';
 import { productSchema } from '../../mongo/schemas/validationSchemas/validationSchemas.js';
 
@@ -11,20 +10,29 @@ const productsRouter = Router();
 
 
 productsRouter.use(jwtAuthMiddleware);
+
 productsRouter.get('/', productsController.getProducts);
+
 productsRouter.get('/:id', productsController.getProductById);
+
 productsRouter.get('/category/:id', productsController.getProductsByCategoryId);
+
 productsRouter.post('/', 
     rbacAuthMiddleware('admin'), 
     validateMiddleware(productSchema), 
     productsController.createProduct
 );
+
 productsRouter.put('/:id', 
     rbacAuthMiddleware('admin'), 
     validateMiddleware(productSchema),
     productsController.updateProduct
 );
-productsRouter.delete('/:id', rbacAuthMiddleware('admin'), productsController.deleteProduct);
+
+productsRouter.delete('/:id', 
+    rbacAuthMiddleware('admin'), 
+    productsController.deleteProduct
+);
 
 
 export default productsRouter;
